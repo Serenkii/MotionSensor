13f4940902e0054998ac17f071e4a10a99a9062e
Serenki <76261221+Serenkii@users.noreply.github.com>
2022-05-05 20:11:06 +0200

    Added functionality to motion sensor Everytime the motion sensor senses motion, the light now turns on for a short duration

--- MotionSensor.ino
+++ MotionSensor.ino
@@ -1,6 +1,10 @@
 #define LED_PIN 2
 #define MOTION_SENSOR 7
 
+static const unsigned int LIGHT_ON_DURATION = 2000;  //duration of time the light stays on after motion (in ms)
+
+unsigned int motionDetected = 0;
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
@@ -10,12 +14,29 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  //Serial.println(digitalRead(MOTION_SENSOR));
-
+  detectMotion();
+  updateLED();
+  
 }
 
+void updateLED() {
+  if (LEDOnCondition()) {
+    digitalWrite(LED_PIN, HIGH);
+    return;
+  }
+  digitalWrite(LED_PIN, LOW);
+}
 
+bool LEDOnCondition() {
+  return motionDetected + LIGHT_ON_DURATION > millis();
+}
+
+void detectMotion() {
+  if (digitalRead(MOTION_SENSOR)) {
+    motionDetected = millis();
+    Serial.println("Motion detected");
+  }
+}
 
 
 
