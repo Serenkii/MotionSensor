c54c7c7bc9a8146fde01ae108eaaae7f6795e402
Serenki <76261221+Serenkii@users.noreply.github.com>
2022-05-05 19:52:56 +0200

    Added the motion sensor

--- MotionSensor.ino
+++ MotionSensor.ino
@@ -1,10 +1,24 @@
 #define LED_PIN 2
+#define MOTION_SENSOR 7
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(MOTION_SENSOR, INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   digitalWrite(LED_PIN, HIGH);
+  //Serial.println(digitalRead(MOTION_SENSOR));
+
 }
+
+
+
+
+
+
+
+
