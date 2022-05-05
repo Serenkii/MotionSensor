f7e38b96e2e4d2f282763333181d567094c887eb
Serenki <76261221+Serenkii@users.noreply.github.com>
2022-05-05 19:06:27 +0200

    Getting to know Arduino again

--- MotionSensor.ino
+++ MotionSensor.ino
@@ -1,9 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-  
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, false);
+  delay(100);
+  digitalWrite(LED_BUILTIN, true);
+  delay(300);
 }
